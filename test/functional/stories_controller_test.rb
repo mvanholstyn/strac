e6666722e787adc9f4c5d79a2bb7b1948eee4ad1
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-01-22 20:22:06 +0000

    Added tests for Story and StoryController. Added svn:ignore for coverage. Added rails_rcov plugin as an external
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@63 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -4,6 +4,7 @@ require 'stories_controller'
 # Re-raise errors caught by the controller.
 class StoriesController; def rescue_action(e) raise e end; end
 
+#TODO: Do I want to update the generated functionals to be more robust?
 class StoriesControllerTest < Test::Unit::TestCase
   fixtures :stories
 
@@ -23,13 +24,13 @@ class StoriesControllerTest < Test::Unit::TestCase
     get :new
     assert_response :success
   end
-  
+
   def test_should_create_story
     old_count = Story.count
-    post :create, :story => { }
+    post :create, :story => { :summary => "My Summary", :description => "My Description" }
     assert_equal old_count+1, Story.count
-    
-    assert_redirected_to story_path(assigns(:story))
+
+    assert_redirected_to stories_path
   end
 
   def test_should_show_story
@@ -41,17 +42,25 @@ class StoriesControllerTest < Test::Unit::TestCase
     get :edit, :id => 1
     assert_response :success
   end
-  
+
   def test_should_update_story
     put :update, :id => 1, :story => { }
-    assert_redirected_to story_path(assigns(:story))
+    assert_redirected_to stories_path
   end
-  
+
   def test_should_destroy_story
     old_count = Story.count
     delete :destroy, :id => 1
     assert_equal old_count-1, Story.count
-    
+
     assert_redirected_to stories_path
+  end
+
+  #TODO: This could be far more robust
+  def test_post_to_reorder_should_attempt_to_reorder
+    Story.expects( :reorder ).with( [ 2, 1 ] ).returns( true )
+
+    post :reorder, :stories => [ 2, 1 ]
+    assert_response :success
   end
 end
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -3,8 +3,75 @@ require File.dirname(__FILE__) + '/../test_helper'
 class StoryTest < Test::Unit::TestCase
   fixtures :stories
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_summary_attribute_should_be_required
+    story = Story.new
+    story.valid?
+
+    assert_instance_of String, story.errors.on( :summary )
+
+    story.summary = "My Summary"
+    story.valid?
+
+    assert_nil story.errors.on( :summary )
+  end
+
+  def test_description_attribute_should_be_required
+    story = Story.new
+    story.valid?
+
+    assert_instance_of String, story.errors.on( :description )
+
+    story.description = "My Description"
+    story.valid?
+
+    assert_nil story.errors.on( :description )
+  end
+
+  def test_points_attribute_should_not_be_required
+    story = Story.new
+    story.valid?
+
+    assert_nil story.errors.on( :points )
+  end
+
+  def test_position_attribute_should_not_be_required
+    story = Story.new
+    story.valid?
+
+    assert_nil story.errors.on( :position )
+  end
+
+  def test_points_attribute_should_be_numerical
+    story = Story.new :points => 'three'
+    story.valid?
+
+    assert_instance_of String, story.errors.on( :points )
+
+    story.points = 3
+    story.valid?
+
+    assert_nil story.errors.on( :points )
+  end
+
+  def test_position_attribute_should_be_numerical
+    story = Story.new :position => 'zero'
+    story.valid?
+
+    assert_instance_of String, story.errors.on( :position )
+
+    story.position = 0
+    story.valid?
+
+    assert_nil story.errors.on( :position )
+  end
+
+  def test_reoreder_should_update_position
+    assert_equal 0, Story.find( 1 ).position
+    assert_equal 1, Story.find( 2 ).position
+
+    assert Story.reorder( [ 2, 1 ] )
+
+    assert_equal 1, Story.find( 1 ).position
+    assert_equal 0, Story.find( 2 ).position
   end
 end
