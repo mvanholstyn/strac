6bcea7cd2a3ccf4b975b1095f2ae5f7dabf32dd7
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-01-22 21:11:48 +0000

    Updated story forms to not include the position attribute. Updated story migration to make description a text field, not a carchar
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@65 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- db/migrate/001_create_stories.rb
+++ db/migrate/001_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration
   def self.up
     create_table :stories do |t|
       t.column :summary, :string
-      t.column :description, :string
+      t.column :description, :text
       t.column :points, :integer
       t.column :position, :integer
       t.column :complete, :boolean
