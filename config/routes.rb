51249217a106db2640b447b4a9fad995e1a4d05a
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-03-07 07:03:55 +0000

    Commented out default routes. All routes should be explicit
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@173 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,6 @@ ActionController::Routing::Routes.draw do |map|
   # map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
 end
