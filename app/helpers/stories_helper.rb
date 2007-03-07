eaa143be8aad39ba5a46297429239c01dbb16a40
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-03-07 07:20:00 +0000

    Updated new hide/show effects to be done mainly client side and to use smart loading (done load if it has already been loaded). This will eliminate alot of server calls and make it much more responsive.
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@175 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,16 @@
 module StoriesHelper
+  def toggle_hide_show_with_smart_loading id_to_show, *ids_to_hide
+    update_page do |page| 
+      page << "if( $('#{id_to_show}').visible() ) {"
+			  page.visual_effect :blind_up, id_to_show, :duration => 0.3
+			  page << "return false;"
+			page << "} else if( $('#{id_to_show}').innerHTML != '' ) {"
+			  ids_to_hide.each do |id_to_hide|
+			    page.visual_effect :blind_up, id_to_hide, :duration => 0.3
+			  end
+			  page.visual_effect :blind_down, id_to_show, :duration => 0.3
+			  page << "return false;"
+			page << "}"
+		end
+  end
 end
