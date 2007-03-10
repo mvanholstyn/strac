dedc45e9f67ea4701a32630c9830c006ca4d305d
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-03-10 15:44:05 +0000

    Added migration for some default group/priv/user data
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@188 b1a0fcd8-281e-0410-b946-bf040eef99c3

---
+++ db/migrate/013_add_default_authentication_data.rb
@@ -0,0 +1,21 @@
+class AddDefaultAuthenticationData < ActiveRecord::Migration
+  def self.up
+    developer_privilege = Privilege.create! :name => 'developer'
+    customer_privilege = Privilege.create! :name => 'customer'
+    user_privilege = Privilege.create! :name => 'user'
+    
+    developer_group = Group.create! :name => 'Developer'
+    developer_group.privileges << user_privilege << developer_privilege
+    
+    customer_group = Group.create! :name => 'Customer'
+    customer_group.privileges << user_privilege << customer_privilege
+    
+    administrator = User.create! :username ='admin', :password => 'password', :password_confirmation => 'password'
+  end
+
+  def self.down
+    Privilege.delete_all
+    Group.delete_all
+    User.delete_all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 12) do
+ActiveRecord::Schema.define(:version => 13) do
 
   create_table "companies", :force => true do |t|
     t.column "name", :string
