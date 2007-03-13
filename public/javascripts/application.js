a05af5e8d09e73c4db8fba0d5d227f21be948fa8
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-03-13 00:21:16 +0000

    Added ScrollPage effect to be used in parallel with another effect which moves an element
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@206 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,25 +10,12 @@ function toggle_hide_show_with_smart_loading( id_to_show, ids_to_hide ) {
 			}
 		);
 		
-		setTimeout( 
-			function() { 
-				var window_top =  window.pageYOffset
-					|| document.documentElement.scrollTop
-					|| document.body.scrollTop
-					|| 0;
-				var window_height = window.innerHeight
-					|| document.body.innerHeight
-					|| 0;
-
-				var element_top = Position.cumulativeOffset( id_to_show )[1];
-				var element_height = id_to_show.getHeight();
-
-				if( element_top + element_height > window_top + window_height ) {
-					window.scrollBy( 0, ( element_top + element_height ) - ( window_top + window_height ) );
-				}
-			}, 500 ); 
-
-		new Effect.BlindDown( id_to_show, { duration: 0.3 } );
+		new Effect.Parallel(
+			[	new Effect.BlindDown( id_to_show, { sync: true } ), 
+				new Effect.ScrollPage( id_to_show, { syne: true } ) ],
+			{ duration: 0.3 }
+		);
+		
 		if( arguments[2] ) {
 			Selector.findChildElements( id_to_show, $A( [ "form" ] ) ).each( 
 				function( form ) { 
@@ -43,3 +30,34 @@ function toggle_hide_show_with_smart_loading( id_to_show, ids_to_hide ) {
 	}
 	return true;
 }
+
+
+Effect.ScrollPage = Class.create();
+Object.extend(Object.extend(Effect.ScrollPage.prototype, Effect.Base.prototype), {
+	initialize: function(element) {
+		this.element = $(element);
+		this.start(arguments[1] || {});
+	},
+	update: function(position) {
+		var window_top =  window.pageYOffset
+				|| document.documentElement.scrollTop
+				|| document.body.scrollTop
+				|| 0;
+
+		var window_height = window.innerHeight
+			|| document.body.innerHeight
+			|| 0;
+
+		var element_top = Position.cumulativeOffset( this.element )[1];
+		var element_height = this.element.getHeight();
+
+		var y_target = ( element_top + element_height ) - window_height;
+		
+		if( element_top + element_height > window_top + window_height ) {
+			if( Position.cumulativeOffset( this.element.parentNode )[1] < y_target ) {
+				y_target = Position.cumulativeOffset( this.element.parentNode )[1];
+			}
+			window.scrollTo( 0, y_target );
+		}
+	}
+});
