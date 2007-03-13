773ce59ee8d245b5262d4c1cd325d45ce462a14f
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-03-13 03:43:29 +0000

    Added validation to Activity model
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@217 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :actor, :class_name => 'User', :foreign_key => 'actor_id'
   belongs_to :affected, :polymorphic => true
+  
+  validates_presence_of :actor_id, :affected_id, :affected_type
 end
