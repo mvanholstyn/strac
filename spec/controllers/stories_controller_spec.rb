37a032dab23093bb0847b672ac263937d0e8ab30
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-07-09 00:16:45 +0000

    added stubs for stories helper and controller specs
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@456 b1a0fcd8-281e-0410-b946-bf040eef99c3

---
+++ spec/controllers/stories_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe StoriesController do
+
+  #Delete this example and add some real ones
+  it "should use StoriesController" do
+    controller.should be_an_instance_of(StoriesController)
+  end
+
+end
