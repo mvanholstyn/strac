ae352166a9edd33315a76bad6f945b838dc7ba3c
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-08-17 04:34:40 +0000

    Updated deployment to new let_deployment plugin. This needs to be tested....
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@533 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,139 +1,45 @@
-# This defines a deployment "recipe" that you can feed to capistrano
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
-
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
+load File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'lwt_deployment', 'recipes')
 
 set :application, "strac"
-set :repository, "http://rails.lotswholetime.com/svn/#{application}/trunk"
+# My repository does not follow my own standard convention...
+# set :repository_host, "svn.example.com"
+set :repository, "http://rails.lotswholetime.com/svn/#{application}/#{repository_path}"
 
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
-
+# Used for web.disable and web.enable
 role :web, "lotswholetime.com"
+# Used for deploy.start, deploy.stop, deploy.restart
 role :app, "lotswholetime.com"
+# Used for deploy.migrate
 role :db, "lotswholetime.com", :primary => true
 
-# =============================================================================
-# OPTIONAL VARIABLES
-# =============================================================================
-set :deploy_to, "/home/mvanholstyn/websites/strac.lotswholetime.com" # defaults to "/u/apps/#{application}"
-# set :user, "flippy"            # defaults to the currently logged in user
-# set :scm, :darcs               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
+# This application deploys to my home directory
+set :deploy_to, "/home/mvanholstyn/websites/strac.lotswholetime.com"
 
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
+# This application does not have staging
+set(:rails_env, :production)
 
-# =============================================================================
-# TASKS
-# =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
-
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
-task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { delete "/tmp/dump.sql" }
-
-  run "mysqldump -u theuser -p thedatabase > /tmp/dump.sql" do |ch, stream, out|
-    ch.send_data "thepassword\n" if out =~ /^Enter password:/
+namespace :deploy do
+  # This application is not on mongrel...
+  desc "Start apache"
+  task :start do
+    sudo "apache2ctl start"
   end
-end
-
-# Tasks may take advantage of several different helper methods to interact
-# with the remote server(s). These are:
-#
-# * run(command, options={}, &block): execute the given command on all servers
-#   associated with the current task, in parallel. The block, if given, should
-#   accept three parameters: the communication channel, a symbol identifying the
-#   type of stream (:err or :out), and the data. The block is invoked for all
-#   output from the command, allowing you to inspect output and act
-#   accordingly.
-# * sudo(command, options={}, &block): same as run, but it executes the command
-#   via sudo.
-# * delete(path, options={}): deletes the given file or directory from all
-#   associated servers. If :recursive => true is given in the options, the
-#   delete uses "rm -rf" instead of "rm -f".
-# * put(buffer, path, options={}): creates or overwrites a file at "path" on
-#   all associated servers, populating it with the contents of "buffer". You
-#   can specify :mode as an integer value, which will be used to set the mode
-#   on the file.
-# * render(template, options={}) or render(options={}): renders the given
-#   template and returns a string. Alternatively, if the :template key is given,
-#   it will be treated as the contents of the template to render. Any other keys
-#   are treated as local variables, which are made available to the (ERb)
-#   template.
-
-desc "Demonstrates the various helper methods available to recipes."
-task :helper_demo do
-  # "setup" is a standard task which sets up the directory structure on the
-  # remote servers. It is a good idea to run the "setup" task at least once
-  # at the beginning of your app's lifetime (it is non-destructive).
-  setup
-
-  buffer = render("maintenance.rhtml", :deadline => ENV['UNTIL'])
-  put buffer, "#{shared_path}/system/maintenance.html", :mode => 0644
-  sudo "killall -USR1 dispatch.fcgi"
-  run "#{release_path}/script/spin"
-  delete "#{shared_path}/system/maintenance.html"
-end
-
-# You can use "transaction" to indicate that if any of the tasks within it fail,
-# all should be rolled back (for each task that specifies an on_rollback
-# handler).
-
-desc "A task demonstrating the use of transactions."
-task :long_deploy do
-  transaction do
-    update_code
-    disable_web
-    symlink
-    migrate
+  
+  desc "Stop apache"
+  task :stop do
+    sudo "apache2ctl stop"
   end
-
-  restart
-  enable_web
-end
-
-desc "Create extra shared items after setup"
-task :after_setup do
-  run "mkdir -p #{shared_path}/config"
-  put IO.read( "#{File.dirname( __FILE__ )}/database.yml" ), "#{shared_path}/config/database.yml"
-end
-
-desc "Symlink in database config, update permissions"
-task :after_symlink do
-  run "ln -s #{shared_path}/config/database.yml #{current_path}/config/database.yml"
-  sudo "chown -R mvanholstyn:www-data #{shared_path}/.."
-  run "chmod -R 770 #{shared_path}/.."
-end
-
-desc "Restart apache"
-task :restart do
-  sudo "apache2ctl restart"
+  
+  desc "Restart apache"
+  task :restart do
+    sudo "apache2ctl restart"
+  end
+  
+  # This application needs special permissions
+  desc "Update permissions"
+  task :update_permissions do
+    sudo "chown -R mvanholstyn:www-data #{shared_path}/.."
+    run "chmod -R 770 #{shared_path}/.."
+  end
+  after "deploy:symlink", "deploy:update_permissions"
 end
