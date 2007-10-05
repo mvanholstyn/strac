6c1ae683233ad4643f871fe5bb37dd11257d3ca8
zdennis <zdennis@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-10-05 00:46:58 +0000

    Adding the stories/ directory.
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@588 b1a0fcd8-281e-0410-b946-bf040eef99c3

---
+++ stories/user_login_story.rb
@@ -0,0 +1,64 @@
+require File.expand_path(File.dirname(__FILE__) + "/all")
+
+Story "User Login", %|
+  As a User
+  I want to be able to login
+  So that I can access projects I have interest in
+|, :type => RailsStory do
+  
+  Scenario "User with invalid credentials" do
+    Given "a user" do
+       @user = Generate.user("bob@jones.com")
+    end
+    When "they try to login" do
+      get "/users/login"
+      submit_form "login_form" do |form|
+        form.user.email_address = @user.email_address
+        form.user.password = "Invalid password!"
+      end
+      response.should be_success
+    end
+    Then "they see an error" do
+      response.should have_tag("#error")
+    end
+    And "remain on the login page" do
+      response.should render_template("users/login")
+    end
+  end
+  
+  Scenario "User with good credentials who has not been activated" do
+    Given "a user" do
+       @user = Generate.user("bob@jones.com", :password=>"password", :password_confirmation=>"password")
+    end
+    When "they try to login" do
+      get "/users/login"
+      submit_form "login_form" do |form|
+        form.user.email_address = @user.email_address
+        form.user.password = "password"
+      end
+    end
+    Then "they see an error" do
+      response.should have_tag("#error")
+    end
+    And "remain on the login page" do
+      response.should render_template("users/login")
+    end
+  end
+
+  Scenario "User with good credentials who has been activated" do
+    Given "a user" do
+       @user = Generate.active_user("bob@jones.com", :password=>"password", :password_confirmation=>"password")
+    end
+    When "they try to login" do
+      get "/users/login"
+      submit_form "login_form" do |form|
+        form.user.email_address = @user.email_address
+        form.user.password = "password"
+      end
+    end
+    Then "they are redirected to to the dashboard page" do
+      response.should redirect_to("/")
+    end
+  end
+
+end
