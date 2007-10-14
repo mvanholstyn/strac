db17a43b6c160c45ebcc21c44a56df47c87f0a93
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2007-10-14 03:15:18 +0000

    Update (all helpers, forgery protection)
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@629 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'a38aedd25d926734dde3ef15f505a98d'
+  
   include ExceptionLoggable
   
   helper :all
