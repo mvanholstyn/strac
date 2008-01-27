5a001a71767e2d566289ceaf78215c4bab78f59a
mvanholstyn <mvanholstyn@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2008-01-27 21:23:03 +0000

    Updated lwt deployment
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@783 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- vendor/plugins/lwt_deployment/recipes/apache.rb
+++ vendor/plugins/lwt_deployment/recipes/apache.rb
@@ -1,8 +1,10 @@
 namespace :apache do
   task :configure do
+    require 'erb'
+    run "umask 02 && mkdir -p #{shared_path}/config"
     template = File.read(File.join(File.dirname(__FILE__), "..", "templates", "config", "virtual_host.conf"))
     result = ERB.new(template).result(binding)
-    put result, "/etc/apache2/sites-available/#{application}-#{rails_env}.conf", :mode => 0644
-    run "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf && ln -s /etc/apache2/sites-available/#{application}-#{rails_env}.conf /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf"
+    put result, "#{shared_path}/config/virtual_host.conf", :mode => 0644
+    # sudo "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf && ln -s #{shared_path}/config/virtual_host.conf /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf"
   end
 end
