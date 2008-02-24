6e9de939369ea5bb69e0ab371c6b5204531bc7e0
zdennis <zdennis@b1a0fcd8-281e-0410-b946-bf040eef99c3>
2008-02-24 01:52:42 +0000

    Fixed an issue with the VelocityCalculator.compute_weighted_average method to use Karn & Partridget's SRTT algorithm. Thanks to Carl Erickson at Atomic Object.
    git-svn-id: http://rails.lotswholetime.com/svn/strac/trunk@869 b1a0fcd8-281e-0410-b946-bf040eef99c3

--- lib/velocity_calculator.rb
+++ lib/velocity_calculator.rb
@@ -1,11 +1,13 @@
 class VelocityCalculator
   ALPHA = 0.5
   
-  def self.compute_weighted_average(values, alpha=ALPHA)
-    return 0 if values.empty?
-    values[1..-1].inject(values[0]) do |t, p|
-      alpha * t + (1-alpha) * p 
+  # This uses the Karn & Patridge SRTT algorithm. http://www.opalsoft.net/qos/TCP-10.htm
+  def self.compute_weighted_average(points, alpha=ALPHA)
+    return 0 if points.empty?
+    points[1..-1].inject(points[0]) do |t, p|
+      (alpha * p) + (1-alpha) * t
     end
   end
   
 end
+
--- spec/objects/velocity_calculator_spec.rb
+++ spec/objects/velocity_calculator_spec.rb
@@ -4,8 +4,12 @@ describe VelocityCalculator, '.compute_weighted_average' do
   def compute_weighted_average(points, alpha)
     VelocityCalculator.compute_weighted_average(points, alpha)
   end
+
+  before do
+    @points = [10, 10, 10, 40, 35, 47, 47]
+  end
   
-  describe "when given no points" do
+  describe "when given an empty array of points" do
     it "returns 0" do
       compute_weighted_average([], 0.7).should == 0
     end
@@ -13,9 +17,22 @@ describe VelocityCalculator, '.compute_weighted_average' do
 
   describe "when given an array of points" do
     it "returns the weighted average based on the given alpha" do
-      alpha = 0.5
-      points = [10, 10, 10, 40, 35, 47, 47]
-      compute_weighted_average(points, alpha).should be_close(42.75, 0.001)
+      alpha = 0.7
+      compute_weighted_average(@points, alpha).should be_close(45.812, 0.001)
+    end
+  end
+  
+  describe "when given a alpha of 0" do
+    it "returns the oldest value" do
+      alpha = 0.0
+      compute_weighted_average(@points, alpha).should be_close(@points.first, 0.001)      
+    end
+  end
+  
+  describe "when given an alpha of 1" do
+    it "returns the most recent value" do
+      alpha = 1.0
+      compute_weighted_average(@points, alpha).should be_close(@points.last, 0.001)      
     end
   end
 end
