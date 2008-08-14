db480f56b97613795a6df5aec30536187e217ac1
Zach Dennis <zach.dennis@gmail.com>
2008-08-14 14:22:36 -0400

    Updated mhs deploy config to use git and remote caching.

--- config/deploy/mhs.rb
+++ config/deploy/mhs.rb
@@ -1,8 +1,10 @@
 set :application, "strac"
-set :repository, "http://rails.lotswholetime.com/svn/#{application}/#{repository_path}"
+set :repository, "git://github.com/mvanholstyn/strac.git"
+set :scm, :git
+set :deploy_via, :remote_cache 
+set :rails_env, :production
 
 role :web, "strac.mutuallyhuman.com"
 role :app, "strac.mutuallyhuman.com"
 role :db, "strac.mutuallyhuman.com", :primary => true
 
-set(:rails_env, :production)
